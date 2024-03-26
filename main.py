8b12f7f5143c42b21944908af19a04bede4ee069
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-03-26 21:43:54 +0530

    edits

--- main.py
+++ main.py
@@ -1,50 +1,74 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from model import ConvRNNModel
+from dataset import CustomDataset
+from train import train_model
+from eval import evaluate_model
+
+# Constants
 num_artist_classes = 23
 num_style_classes = 27
 num_genre_classes = 10
-
-# For conv. layer
-in_channels = 3  # RGB
-out_channels = 64  # No. of kernels
-kernel_size = 3  # Size of kernel
-
-# For recurr. layer
-input_size = 64  # Input size to recurrent layer (output size of conv layer)
-hidden_size = 128  # Hidden units
-num_layers = 2  # No. of layers in the recurrent 
-batch_size = 32  # Number of samples
-sequence_length = 10  # Number of images for single sequence
-
-artist_model = ConvRNNModel(num_artist_classes)
-style_model = ConvRNNModel(num_style_classes)
-genre_model = ConvRNNModel(num_genre_classes)
-
-print("-------ARTIST-------", artist_model)
-print("-------STYLE-------", style_model)
-print("-------GENRE-------", genre_model)
-
-
-# Might expriment with these 
-artist_criterion = nn.CrossEntropyLoss()
-style_criterion = nn.CrossEntropyLoss()
-genre_criterion = nn.CrossEntropyLoss()
-
-artist_optimizer = optim.Adam(artist_model.parameters(), lr=0.001)
-style_optimizer = optim.Adam(style_model.parameters(), lr=0.001)
-genre_optimizer = optim.Adam(genre_model.parameters(), lr=0.001)
-
+in_channels = 3
+out_channels = 64
+kernel_size = 3
+input_size = 64
+hidden_size = 128
+num_layers = 2
+batch_size = 32
+sequence_length = 10
 num_epochs = 10
+learning_rate = 0.001
 
-train_model(artist_model, artist_train_dataloader, artist_val_dataloader, artist_criterion, artist_optimizer, num_epochs)
-train_model(style_model, style_train_dataloader, style_val_dataloader, style_criterion, style_optimizer, num_epochs)
-train_model(genre_model, genre_train_dataloader, genre_val_dataloader, genre_criterion, genre_optimizer, num_epochs)
+# File paths
+path_to_folder = '/Users/sarthakkapila/Desktop/wikiart_csv/'
 
+# Define transformations
+transform = transforms.Compose([
+    transforms.Resize((416, 416)),
+    transforms.ToTensor(),
+])
 
+# Load datasets
+train_datasets = {
+    'artist': CustomDataset(path_to_folder + 'artist_train_modified_final.csv', transform=transform),
+    'style': CustomDataset(path_to_folder + 'style_train_modified_final.csv', transform=transform),
+    'genre': CustomDataset(path_to_folder + 'genre_train_modified_final.csv', transform=transform)
+}
 
-style_accuracy = evaluate_model(style_model, style_test_loader)
-print(f'Style Model Accuracy: {style_accuracy:.2f}%')
+val_datasets = {
+    'artist': CustomDataset(path_to_folder + 'artist_val_modified_final.csv', transform=transform),
+    'style': CustomDataset(path_to_folder + 'style_val_modified_final.csv', transform=transform),
+    'genre': CustomDataset(path_to_folder + 'genre_val_modified_final.csv', transform=transform)
+}
 
-artist_accuracy = evaluate_model(artist_model, artist_test_loader)
-print(f'Artist Model Accuracy: {artist_accuracy:.2f}%')
+# Create data loaders
+train_loaders = {task: DataLoader(dataset, batch_size=batch_size, shuffle=True) for task, dataset in train_datasets.items()}
+val_loaders = {task: DataLoader(dataset, batch_size=batch_size, shuffle=True) for task, dataset in val_datasets.items()}
 
-genre_accuracy = evaluate_model(genre_model, genre_test_loader)
-print(f'Genre Model Accuracy: {genre_accuracy:.2f}%')
+# Create models
+models = {
+    'artist': ConvRNNModel(num_artist_classes),
+    'style': ConvRNNModel(num_style_classes),
+    'genre': ConvRNNModel(num_genre_classes)
+}
+
+# print("-------ARTIST-------", artist_model)
+# print("-------STYLE-------", style_model)
+# print("-------GENRE-------", genre_model)
+
+# Create optimizers and loss functions
+optimizers = {task: optim.Adam(model.parameters(), lr=learning_rate) for task, model in models.items()}
+criterions = {task: nn.CrossEntropyLoss() for task in models}
+
+# Train models
+for task in models:
+    train_model(models[task], train_loaders[task], val_loaders[task], criterions[task], optimizers[task], num_epochs)
+
+# Evaluate models
+for task in models:
+    accuracy = evaluate_model(models[task], val_loaders[task])
+    print(f'{task.capitalize()} Model Accuracy: {accuracy:.2f}%')
